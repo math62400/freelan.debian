4b926b3a1bcd07f50b8897bf81cd1236ed1c7195
Julien Kauffmann <julien.kauffmann@freelan.org>
2012-03-13 15:37:26 +0100

    Fixed init.d script

--- debian/freelan.init.d
+++ debian/freelan.init.d
@@ -1,27 +1,34 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          freelan
-# Required-Start:    $network $local_fs
-# Required-Stop:
+# Required-Start:    $network $remote_fs $syslog
+# Required-Stop:     $network $remote_fs $syslog
+# Should-Start:      network-manager
+# Should-Stop:       network-manager
+# X-Start-Before:    $x-display-manager gdm kdm xdm wdm ldm sdm nodm
+# X-Interactive:     true
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Peer-to-peer virtual private network daemon
-# Description:       Peer-to-peer virtual private network daemon
+# Short-Description: Freelan VPN service
+# Description: This script will start Freelan tunnels as specified in /etc/default/freelan and /etc/freelan/*.conf
 ### END INIT INFO
 
 # Author: Julien Kauffmann <julien.kauffmann@freelan.org>
 
-# PATH should only include /usr/* if it runs after the mountnfs.sh script
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
-DESC=freelan             # Introduce a short description here
-NAME=freelan             # Introduce the short server's name here
-DAEMON=/usr/sbin/freelan # Introduce the server's location here
-DAEMON_ARGS=""             # Arguments to run the daemon with
+PATH=/sbin:/usr/sbin
+DESC="peer-to-peer virtual private network daemon"
+NAME="freelan"
+DAEMON=/usr/sbin/freelan
+DAEMON_ARGS=""
 PIDFILE=/var/run/$NAME.pid
 SCRIPTNAME=/etc/init.d/$NAME
+CONFIG_DIR=/etc/$NAME
 
 # Exit if the package is not installed
 [ -x $DAEMON ] || exit 0
+
+# Defaults
+CONFIGURATIONS="freelan"
 
 # Read configuration variable file if it is present
 [ -r /etc/default/$NAME ] && . /etc/default/$NAME
@@ -33,36 +40,30 @@ SCRIPTNAME=/etc/init.d/$NAME
 # Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
 . /lib/lsb/init-functions
 
-#
-# Function that starts the daemon/service
-#
-do_start()
+do_start_instance()
 {
 	# Return
 	#   0 if daemon has been started
 	#   1 if daemon was already running
 	#   2 if daemon could not be started
-	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON --test > /dev/null \
+	start-stop-daemon --start --quiet --pidfile $PIDFILE.$CONFIG --exec $DAEMON --test > /dev/null \
 		|| return 1
-	start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON -- \
-		$DAEMON_ARGS \
+	start-stop-daemon --start --quiet --pidfile $PIDFILE.$CONFIG --exec $DAEMON -- \
+		$DAEMON_ARGS -c $CONFIG_FILE \
 		|| return 2
 	# Add code here, if necessary, that waits for the process to be ready
 	# to handle requests from services started subsequently which depend
 	# on this one.  As a last resort, sleep for some time.
 }
 
-#
-# Function that stops the daemon/service
-#
-do_stop()
+do_stop_instance()
 {
 	# Return
 	#   0 if daemon has been stopped
 	#   1 if daemon was already stopped
 	#   2 if daemon could not be stopped
 	#   other if a failure occurred
-	start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE --name $NAME
+	start-stop-daemon --stop --quiet --retry=TERM/30/KILL/5 --pidfile $PIDFILE.$CONFIG --name $NAME
 	RETVAL="$?"
 	[ "$RETVAL" = 2 ] && return 2
 	# Wait for children to finish too if this is a daemon that forks
@@ -71,24 +72,53 @@ do_stop()
 	# that waits for the process to drop all resources that could be
 	# needed by services started subsequently.  A last resort is to
 	# sleep for some time.
-	start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
-	[ "$?" = 2 ] && return 2
+	#start-stop-daemon --stop --quiet --oknodo --retry=0/30/KILL/5 --exec $DAEMON
+	#[ "$?" = 2 ] && return 2
 	# Many daemons don't delete their pidfiles when they exit.
-	rm -f $PIDFILE
+	rm -f $PIDFILE.$CONFIG
 	return "$RETVAL"
 }
 
 #
-# Function that sends a SIGHUP to the daemon/service
+# Function that starts the daemon/service
 #
-do_reload() {
-	#
-	# If the daemon can reload its configuration without
-	# restarting (for example, when it is sent a SIGHUP),
-	# then implement that here.
-	#
-	start-stop-daemon --stop --signal 1 --quiet --pidfile $PIDFILE --name $NAME
-	return 0
+do_start()
+{
+	for CONFIG in $CONFIGURATIONS; do
+		CONFIG_FILE="$CONFIG_DIR/$CONFIG.conf"
+		if test -e "$CONFIG_FILE"; then
+			log_daemon_msg "Starting $CONFIG..."
+			do_start_instance
+
+			RETVAL="$?"
+			[ "$RETVAL" = 2 ] && return 2
+			[ "$RETVAL" = 1 ] && log_warning_msg "$CONFIG is already running."
+		else
+			log_failure_msg "No such file: $CONFIG_FILE"
+			return 2
+		fi
+	done
+}
+
+#
+# Function that stops the daemon/service
+#
+do_stop()
+{
+	for CONFIG in $CONFIGURATIONS; do
+		CONFIG_FILE="$CONFIG_DIR/$CONFIG.conf"
+		if test -e "$CONFIG_FILE"; then
+			log_daemon_msg "Stopping $CONFIG..."
+			do_stop_instance
+
+			RETVAL="$?"
+			[ "$RETVAL" = 2 ] && return 2
+			[ "$RETVAL" = 1 ] && log_warning_msg "$CONFIG is already stopped."
+		else
+			log_failure_msg "No such file: $CONFIG_FILE"
+			return 2
+		fi
+	done
 }
 
 case "$1" in
@@ -111,15 +141,6 @@ case "$1" in
   status)
        status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
        ;;
-  #reload|force-reload)
-	#
-	# If do_reload() is not implemented then leave this commented out
-	# and leave 'force-reload' as an alias for 'restart'.
-	#
-	#log_daemon_msg "Reloading $DESC" "$NAME"
-	#do_reload
-	#log_end_msg $?
-	#;;
   restart|force-reload)
 	#
 	# If the "reload" option is implemented then remove the
